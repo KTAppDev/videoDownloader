ab72f6688ab89fd11d7b54abfcbccabb2ef0ba6f
Ken Taylor <kentaylorappdev@gmail.com>
2022-04-09 12:51:53 -0400

    sorting out RE

--- read_from_file.py
+++ read_from_file.py
@@ -1,16 +1,17 @@
 import re
 REXP = 'https?://(?:[-\w.]|(?:%[\da-fA-F]{2}))+/[-\w.]+'
+REXP2 = r'(http|ftp|https):\/\/([\w_-]+(?:(?:\.[\w_-]+)+))([\w.,@?^=%&:\/~+#-]*[\w@?^=%&\/~+#-])'
 
 def read_urls_from_file():
     list_of_urls = []
     try:
         with open("example.txt") as file:
             for line in file:
-                url = re.findall(REXP, line)
+                url = re.findall(REXP2, line)
                 if not url:  # check if the current list is empty. this is insane to me right now lol, compared to how i was gonna check
                     continue
                 else:
-                    list_of_urls.append(url[0])
+                    list_of_urls.append(url)
 
     except Exception as e:
         print(e)
