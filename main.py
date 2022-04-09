40920f67a438613a9e9da55ca8ef0002af3fe4ad
Ken Taylor <kentaylorappdev@gmail.com>
2022-04-09 11:10:58 -0400

    good start

--- main.py
+++ main.py
@@ -1,27 +1,8 @@
-import tic as tic
+
 import ut as ut
-
-
-# This project download videos from famous video sharing platforms
-
-# lnk = 'The link to the tiktok you wish to download'
-
-# new features will be added soon including Twitter, Facebook and Instagram
-
-# def tw():
-#     link = link = input("Please enter the link of the video you would like to download")
-
-
-def fb():
-    link = input("Please enter the link of the video you would like to download")
-
-
-def ig():
-    link = input("Please enter the link of the video you would like to download")
 
 # calling the Youtube file
 # ut.ut()
+# ut.download_playlist()
+ut.searchtube('give them a update')
 
-# calling the TikTok file
-# tic.Get_Video_Name(tic.Get_VideoID(lnk))
-# tic.Download_Video(tic.Get_VideoID(lnk))
--- ut.py
+++ ut.py
@@ -1,25 +1,35 @@
 from pytube import YouTube
+from pytube import Search
+from pytube import Playlist
 
 
-def ut():
-    link = input("Please enter the link of the video you would like to download")
+def ut(link):
     yt = YouTube(link)
-    print(yt.streams.filter(file_extension='mp4'))
-
-    # This selects the stream with this ID it is set to 720p with audio
+    yt.streams.filter(only_audio=True)
     print("Starting download....")
-    stream = yt.streams.get_by_itag(22)
+    stream = yt.streams.get_by_itag(140)
     stream.download()
     print("Download complete!")
 
-    # This selects the stream with this ID it is set to 480p with audio
-    # print("Starting download....")
-    # stream = yt.streams.get_by_itag(135)
-    # stream.download()
-    # print("Download complete!")
 
-    # This selects the stream with this ID it is set to 1080p but this API does not download 1080p videos with sound
-    # stream = yt.streams.get_by_itag(137)
-    # print("Starting download....")
-    # stream.download()
-    # print("Download complete!")
+def searchtube(txt):
+    video_list = []
+    s = Search(f'{txt} clean')
+    for obj in s.results:
+        x = str(obj) #in the future see if theyy have an easier way to use these youtube obj in search results. I doubt what i'm doing is the easy way lol
+        video_id = x[x.rfind('=')+1:].strip('>')
+        video_url = f'https://www.youtube.com/watch?v={video_id}'
+        video_list.append(video_url)
+    return video_list
+
+
+def download_playlist():
+    # print("Insert the link:")
+    # link = input("")
+    playlist = Playlist('https://youtube.com/playlist?list=PLrkXnid3eJj3rS-KUHIYhTWWxtgcweIPD')
+    print('*' * 40)
+    print(f'Playlist contains {len(playlist)} items')
+    print('*' * 40)
+    for url in playlist[:3]:
+        # YouTube(url).streams.filter(only_audio=True).first().download()
+        ut(url)
