3272ace62b9381e85a7391748c5fa611532d73b2
Ken Taylor <kentaylorappdev@gmail.com>
2022-05-07 11:53:47 -0400

    looking to make this gui standalone, need to figure out how to fix path to files with kivy file

--- gui.py
+++ gui.py
@@ -1,3 +1,6 @@
+import os
+import sys
+
 from kivy.app import App
 from kivy.uix.gridlayout import GridLayout
 from kivy.uix.label import Label
@@ -7,8 +10,7 @@ from kivy.uix.textinput import TextInput
 from kivy.uix.widget import Widget
 import yt_downloader
 from kivy.properties import ObjectProperty
-from kivy.core.window import Window
-Window.size = (550, 600)
+
 
 class MyGrid(Widget):
     link = ObjectProperty(None)
@@ -32,11 +34,11 @@ class MyGrid(Widget):
 class YoutubeDownloader(App):
     def build(self):
         self.window = GridLayout()
-        self.window.cols = 1
-        self.window.size_hint = (0.6, 0.7)
-        self.window.pos_hint = {'center_x': 0.5, 'center_y': 0.5}
-
-        self.window.add_widget(Image(source='ytlogo.png'))
+        # self.window.cols = 1
+        # self.window.size_hint = (0.6, 0.7)
+        # self.window.pos_hint = {'center_x': 0.5, 'center_y': 0.5}
+        # logo = self.resource_path('ytlogo.png')
+        # self.window.add_widget(Image(source=logo))
 
         self.update_label = Label(
             text='Updates will appear here',
@@ -72,6 +74,11 @@ class YoutubeDownloader(App):
     def download_button_action(self, instance):
         self.update_label.text = f'downloading {self.link.text}'
         yt_downloader.youtube_single_download(yt_downloader.searchtube(self.link.text), self.op_input.text)
+
+    def resource_path(self, relative_path):
+        if hasattr(sys, '_MEIPASS'):
+            return os.path.join(sys._MEIPASS, relative_path)
+        return os.path.join(os.path.abspath("."), relative_path)
 
 
 if __name__ == "__main__":
