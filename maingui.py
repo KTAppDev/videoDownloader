0466e7a7963f1f080b58b53512e028935065b18d
Ken Taylor <kentaylorappdev@gmail.com>
2022-05-10 01:50:19 -0400

    Ypoooo can't believe how i renamed thelast downloaded song, i need to put comments all through this

--- maingui.py
+++ maingui.py
@@ -24,6 +24,13 @@ def resource_path(relative_path):
     return os.path.join(os.path.abspath("."), relative_path)
 
 
+def rename_file(song_path): #This is my ver own rename function, not sure if this is how everyong does it but it works for me
+    print(song_path)
+    base, ext = song_path.split('.mp4')
+    new_file = base + '.mp3'
+    os.rename(song_path, new_file)
+
+
 class BoxLayoutUI(BoxLayout):
     link = ObjectProperty(None)
     op_input = ObjectProperty(None)
@@ -34,23 +41,22 @@ class BoxLayoutUI(BoxLayout):
     yt_logo = resource_path('ytlogo.png')
 
     def download_button_action(self):
+
         desktop = os.path.expanduser("~/Desktop/")
         if self.link.text == '':
             self.update_label.text = 'ERROR - Please enter a song name and artiste'
             return
+        self.update_label.text = 'Searching...'
         if self.op_input.text == '':
-            self.op_input.text = 'Youtube'
+            self.op_input.text = 'Youtube' #Default folder
         self.path_to_last_song = desktop + self.op_input.text # Made this so i ca reuse it below
-        self.update_label.text = f'Downloaded {yt_downloader.youtube_single_download(yt_downloader.searchtube(self.link.text), self.path_to_last_song)}'
-        self.rename_file(self.path_to_last_song + '/' + self.update_label.text[11:] + '.mp4')
+        self.update_label.text = f'Downloaded {yt_downloader.youtube_single_download(yt_downloader.searchtube(self.link.text), self.path_to_last_song)}' #this returns the title and thats the same as the song title
+        try:
+            rename_file(self.path_to_last_song + '/' + self.update_label.text[11:] + '.mp4')  # remove the word downloaded 11 characters, its the title so i add mp4
+        except Exception as e:
+            print('could not rename file because' + e)
         self.link.text = ''
         self.op_input.text = ''
-
-    def rename_file(self, song_path):
-        print(song_path)
-        base, ext = song_path.split('.')
-        new_file = base + '.mp3'
-        os.rename(song_path, new_file)
 
 
 class YoutubeDownloader(App):
